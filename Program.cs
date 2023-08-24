74d53dc953e68c409d265b13b0795218b4c4c298
Laura <Laura@DESKTOP-BESSB6E>
2023-08-24 21:47:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Iveskite metu skaiciu: ");
+int numberOfYear = Convert.ToInt32(Console.ReadLine());
+if ((numberOfYear % 4 == 0 && numberOfYear % 100 != 0) || numberOfYear % 400 == 0)
+{ 
+    Console.WriteLine("Tai yra keliamieji metai");
+}
+else
+{
+    Console.WriteLine("Tai nėra keliamieji metai");
+}
